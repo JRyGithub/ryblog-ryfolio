6b1d64577b742ce2a7afd8e014124c1cf70aa025
JRyGithub <nzryand@gmail.com>
2023-09-06 20:13:25 +0200

    fix: remove gif, simplify contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,11 +5,11 @@ import ImageWithLink from "./components/ImageWithLink";
 const Contact = () => {
   return (
     <div className="flex flex-col justify-center items-center m-5">
-      <div className="m-5 md:w-fit max-w-2xl p-5 bg-computer-background bg-cover rounded-lg min-h-[50vh]">
-        <div className="flex flex-col gap-2 justify-center text-center glassMorphism p-5 rounded-md text-primary-content font-bold">
+      <div className="m-5 md:w-fit max-w-2xl bg-neutral rounded-md min-h-[50vh] flex flex-col-reverse md:flex-row">
+        <div className="flex flex-col h-3/4 md:h-auto md:w-3/4 gap-2 justify-between text-center glassMorphism p-5 rounded-md text-primary-content font-bold">
           <h1 className="text-5xl font-bold bg-gradient-to-r from-primary via-secondary to-accent text-transparent bg-clip-text animate-gradient bg-300% underline underline-offset-4">Feel free to reach out!</h1>
           <div>Do you love tacky colours and 90s graphics? Fear not, you have your developer 🔥</div>
-          <div>But seriously if you want to discuss IT, jobs, web application ideas, Leeds United or anything at all. Contact me through one of the methods below!</div>
+          <div>But seriously if you want to discuss IT, jobs, web application ideas, Leeds United or anything at all. Contact me through one of the methods below or click the folder to copy my email!</div>
           <div className="flex flex-row gap-2 justify-center items-center">
             <CopyTextButton textToCopy="nzryland@gmail.com" toolTip="Copy Email">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -19,8 +19,8 @@ const Contact = () => {
             <ImageWithLink link="https://github.com/jRyGithub" src="/github-mark-white.png" alt="Github Logo" width={24} height={24} />
             <ImageWithLink link="https://www.linkedin.com/in/joshua-ryland/" src="/LI-In-Bug.png" alt="LinkedIn Logo" width={24} height={24} />
           </div>
-          <div className="w-100 h-0 pb-[56%] relative"><iframe src="https://giphy.com/embed/ZZiLDJ98R2GOY" width="100%" height="100%" className="absolute giphy-embe" frameBorder="0" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/funny-random-house-ZZiLDJ98R2GOY">via GIPHY</a></p>
         </div>
+        <div className="bg-computer-background bg-cover w-full h-32 md:h-auto md:w-1/4 rounded-t-md md:rounded-r-md" />
       </div>
     </div>
   );
