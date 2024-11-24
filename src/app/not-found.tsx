0b4a5b795f98fa8c2c5837bc220eea7518986e69
JRyGithub <jryland@protonmail.com>
2024-11-24 21:50:35 +0000

    feat: simplify WIP
    @

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,51 +1,20 @@
-import Image from 'next/image';
-import Header from './components/header/header';
-import TextLink from './components/textLink/textLink';
+import Spline from "@splinetool/react-spline";
 
-const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#969393" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-const toBase64 = (str: string) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
 
 export default function NotFound() {
   return (
-    <main className="flex flex-col min-h-screen bg-gradient-to-bl from-base-100 from-1% via-base-200 via-15% to-base-300 to-90% font-mono">
-      <Header />
-      <main className="flex flex-col flex-grow min-h-100 justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-2">
-          <Image
-            src={`https://www.robohash.org/${Math.floor(
-              Math.random() * 100 + 1,
-            )}?set=set4`}
-            alt="A picture of cat!"
-            width={400}
-            height={400}
-            priority
-            placeholder='blur'
-            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(400, 400))}`}
-          />
-          <h1 className="text-xl">404</h1>
-          <span className="text-md text-center">
-            Lots coming soon so watch this space 😃
-          </span>
-          <br></br>
-          <TextLink href="/">Return Home</TextLink>
-        </div>
-      </main>
-    </main>
+       <main className="flex min-h-screen flex-col items-center justify-center">
+         <div className="h-screen w-screen relative">
+           <div className="absolute m-4 md:m-10 text-white text-md font-thin flex flex-col gap-1">
+             <span>Joshua Ryland</span>
+             <span className='italic'>Lead Software Developer</span>
+           </div>
+           <div className="absolute bottom-2 right-0 w-44 h-12 bg-[#1c232b]" />
+           <Spline
+             className='h-full'
+             scene="https://prod.spline.design/zgSriR078vW2ueYZ/scene.splinecode" 
+           />
+         </div>
+       </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,21 @@
-import ThemeToggle from "./components/header/molecules/themeToggle";
-import TextLink from "./components/textLink/textLink";
-
-export const revalidate = 86400; // 24 Hours
+import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <main className="bg-clip-padding backdrop-filter bg-opacity-60 flex h-screen w-full items-center justify-center rounded-md bg-gradient-to-bl from-base-100 from-1% via-base-200 to-base-300 to-90% drop-shadow-md md:h-[65vh] md:w-[50rem]">
-        <div className="flex h-full w-full flex-col justify-between box-border p-4 text-content">
-          <div className="flex flex-col justify-start">
-            <div className="flex flex-row justify-between">
-              <div>
-                <h6 className="text-left text-base font-bold leading-none">
-                  Joshua
-                </h6>
-                <h6 className="text-left text-base font-bold leading-none">
-                  Ryland
-                </h6>
-              </div>
-              <div>
-                <ThemeToggle />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <h3 className="text-center font-bold leading-none text-3xl">
-              Just another developer <TextLink href="timeline">portfolio</TextLink>...
-            </h3>
-            <h3 className="text-center font-bold leading-none text-3xl">
-              and a <TextLink href="blog">blog.</TextLink>
-            </h3>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="p flex flex-col justify-start pb-2">
-              <h6 className="text-left text-base font-bold leading-none">
-                Paris, France
-              </h6>
-              <h6 className="mb-0 text-left text-base font-bold leading-none">
-                London, England
-              </h6>
-              <h6 className="mb-0 text-left text-base font-bold leading-none">
-                Remote, My Study
-              </h6>
-            </div>
-            <div className="p flex flex-col justify-start pb-2">
-              <h6 className="text-right text-base font-bold leading-none">
-                Full Stack Developer
-              </h6>
-              <h6 className="mb-0 text-right text-base font-bold leading-none">
-                Software Engineer
-              </h6>
-              <h6 className="mb-0 text-right text-base font-bold leading-none">
-                Project Management
-              </h6>
-            </div>
-          </div>
+      <div className="h-screen w-screen relative">
+        <div className="absolute m-4 md:m-10 text-white text-md font-thin flex flex-col gap-1">
+          <span>Joshua Ryland</span>
+          <span className='italic'>Lead Software Developer</span>
         </div>
-      </main>
+        <div className="absolute bottom-2 right-0 w-44 h-12 bg-[#1c232b]" />
+        <Spline
+          className='h-full bg-[#1c232b]'
+          scene="https://prod.spline.design/zgSriR078vW2ueYZ/scene.splinecode"
+        />
+      </div>
     </main>
   );
 }
+
+export const revalidate = 86400; // 24 Hours
