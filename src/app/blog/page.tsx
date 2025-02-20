456576db78a0564e0b744b05764520635566d0ee
JRyGithub <jryland@protonmail.com>
2025-02-20 19:16:52 +0000

    feat: ready for rejig

--- src/app/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getBlogsMeta } from "@/lib/blogs/blogs";
-import Blogs from "./components/Blogs";
-export const revalidate = 86400; // 24 Hours
-
-export default async function () {
-  const blogs = await getBlogsMeta();
-
-  return <Blogs blogsMeta={blogs} />
-}
